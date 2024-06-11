a7e7f42404500b016f302d032dd45d4885939f3e
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-11 15:36:11 +0700

    salt func to hash password

---
+++ PasswordGenerator/src/main/java/Salt.java
@@ -0,0 +1,21 @@
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
+public class Salt {
+    private static String getSalt()
+            throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        // Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+        // Create array for salt
+        byte[] salt = new byte[16];
+
+        // Get a random salt
+        sr.nextBytes(salt);
+
+        // return salt
+        return salt.toString();
+    }
+}
