3a0fdb443328e87f6e883f5d0a3d8adbc17c336c
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-11 14:08:25 +0700

    ask for input

--- PasswordGenerator/src/main/java/PasswordGenerator.java
+++ PasswordGenerator/src/main/java/PasswordGenerator.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PasswordGenerator {
+    private static ArrayList<Option> options = new ArrayList<>();
+    private static int length = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Length: ");
+        length = sc.nextInt();
+        System.out.println("Numbers (Y/N): ");
+        if (sc.next().toLowerCase() == "y"){
+            options.add(Option.Numbers);
+        }
+        System.out.println("Symbols (Y/N): ");
+        if (sc.next().toLowerCase() == "y")
+            options.add(Option.Symbols);
+
+    }
 }
