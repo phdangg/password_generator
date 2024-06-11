61e4cab26f8016e26b0d6c4a78365fe045cfa41c
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-11 17:41:36 +0700

    catch ex and change require input

--- PasswordGenerator/src/main/java/PasswordGenerator.java
+++ PasswordGenerator/src/main/java/PasswordGenerator.java
@@ -2,25 +2,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PasswordGenerator {
-    private static final ArrayList<Option> options = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        do {
-            System.out.print("Length: ");
-            int length = sc.nextInt();
-            System.out.println("Numbers (Y/N): ");
-            if (sc.next().equalsIgnoreCase("y")) {
-                options.add(Option.Numbers);
-            }
-            System.out.println("Symbols (Y/N): ");
-            if (sc.next().equalsIgnoreCase("y"))
-                options.add(Option.Symbols);
+        try {
+            do {
+                System.out.println("Email:");
+                String email = sc.next();
+                System.out.println("Master Password:");
+                String masterPassword = sc.next();
+                System.out.println("Character Set (default: d):");
+                String characterSet = sc.next();
+                System.out.println("Length:");
+                int length = sc.nextInt();
+                System.out.println("Your password:");
+                System.out.println(Generator.getPassword(email, masterPassword, length,characterSet));
 
-            Generator generator = new Generator(length, options);
-            System.out.println(generator);
-
-            System.out.print("Another? (Y/N): ");
-        } while (sc.next().equalsIgnoreCase("y"));
+                System.out.print("Another? (Y/N): ");
+            } while (sc.next().equalsIgnoreCase("y"));
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
