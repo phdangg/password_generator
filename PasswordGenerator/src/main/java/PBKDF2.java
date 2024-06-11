800eded4f2e252882e9173ff690d6381b54c3f85
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-11 17:16:07 +0700

    PBKDF2 (password-based-key-derivation-function-2)

---
+++ PasswordGenerator/src/main/java/PBKDF2.java
@@ -0,0 +1,40 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
+
+public class PBKDF2 {
+    public static String getPBKDF2WithSalt(String secret){
+        try {
+            int iterations = 1000;
+            char[] chars = secret.toCharArray();
+            byte[] salt = Salt.getSalt();
+
+            PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64*8);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            byte[] hash = skf.generateSecret(spec).getEncoded();
+            return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e){
+            System.out.println(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException
+    {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
+    }
+}
