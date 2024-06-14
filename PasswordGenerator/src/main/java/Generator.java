f47f13cf5cc72abf9a8b8fb157bdb6f412adcf31
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-14 19:27:11 +0700

    put hashing step into a func

--- PasswordGenerator/src/main/java/Generator.java
+++ PasswordGenerator/src/main/java/Generator.java
@@ -5,15 +5,16 @@ import java.nio.charset.StandardCharsets;
 
 
 public class Generator {
+    private byte[] hash;
+    private int passwordLength;
     private static final String DEFAULT_CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
 
-    public static String getPassword(String email, String masterPassword, int PASSWORD_LENGTH, String CHARACTER_SET) {
-        if (email == null || masterPassword == null) return null;
-        if (CHARACTER_SET.equals("d"))
-            CHARACTER_SET = DEFAULT_CHARACTER_SET;
-
-        // Get salt
-        byte[] salt = Salt.getSalt(email);
+    Generator(String masterPassword, int passwordLength, String ...metadata){
+        this.hash = hashing(masterPassword,metadata);
+        this.passwordLength = passwordLength;
+    }
+    private byte[] hashing(String masterPassword, String ...metadata){
+        byte[] salt = Salt.getSalt(metadata);
 
         // Perform PBKDF2 hashing
         byte[] PBKDF2Secret = PBKDF2.getPBKDF2WithSalt(masterPassword,salt);
@@ -23,12 +24,32 @@ public class Generator {
         byte[] salt16Bytes = Salt.getSalt16Bytes(PBKDF2Secret);
 
         // Perform BCrypt hashing
-        byte[] bcryptHash = BCrypt.withDefaults().hash(10, salt16Bytes, masterPassword.getBytes(StandardCharsets.UTF_8));
+        return BCrypt.withDefaults().hash(10, salt16Bytes, masterPassword.getBytes(StandardCharsets.UTF_8));
+    }
+    //A template is a string generated for every password based
+    //on the character set. E.g., a password with at least one
+    //number, one lowercase character and a length of 10.
+    // Whereas "n" denotes a number, "a" denotes a lowercase
+    // letter and x denotes any symbol from the
+    //character set.
+    private static String generateTemplate(int PASSWORD_LENGTH){
+        StringBuilder template = new StringBuilder();
+
+        return template.toString();
+    }
+
+    public String getPassword(String email, String masterPassword, int PASSWORD_LENGTH, String CHARACTER_SET) {
+        if (email == null || masterPassword == null) return null;
+        if (CHARACTER_SET.equals("d"))
+            CHARACTER_SET = DEFAULT_CHARACTER_SET;
+
+        // Get salt
+
 
         // Remove prefix and salt from BCrypt result to get the final byte array
 
-        byte[] finalByteArray = new byte[bcryptHash.length-7];
-        System.arraycopy(bcryptHash,7,finalByteArray,0,finalByteArray.length);
+        byte[] finalByteArray = new byte[hash.length-7];
+        System.arraycopy(hash,7,finalByteArray,0,finalByteArray.length);
 
         // Fill the template with chosen characters
         StringBuilder finalPassword = new StringBuilder();
