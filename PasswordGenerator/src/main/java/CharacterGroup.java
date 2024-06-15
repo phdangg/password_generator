c5023e9d8fed65b057ae663d324a1833164fd1fb
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 12:43:27 +0700

    Character group and set

---
+++ PasswordGenerator/src/main/java/CharacterGroup.java
@@ -0,0 +1,3 @@
+public enum CharacterGroup {
+    UPPER,LOWER,NUMBER,SYMBOL
+}
---
+++ PasswordGenerator/src/main/java/CharacterSet.java
@@ -0,0 +1,6 @@
+public class CharacterSet {
+    public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    public static final String SYMBOL = "#!\"~|@^°$%&/()[]{}=-_+*<>;:.";
+    public static final String NUMBER = "0123456789";
+}
