7e31bb07eda6b5c3f8b2ba29e94ececb95696533
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 13:32:39 +0700

    add options to characterset

--- PasswordGenerator/src/main/java/PasswordGenerator.java
+++ PasswordGenerator/src/main/java/PasswordGenerator.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PasswordGenerator {
 
     public static void main(String[] args) {
+        CharacterGroup[] groups = {
+                CharacterGroup.UPPER,
+                CharacterGroup.LOWER,
+                CharacterGroup.SYMBOL,
+                CharacterGroup.NUMBER
+        };
+
+        String[] prompts = {
+                "Uppercase (y/n): ",
+                "Lowercase (y/n): ",
+                "Symbol (y/n): ",
+                "Number (y/n): "
+        };
         Scanner sc = new Scanner(System.in);
         try {
             do {
@@ -10,13 +24,17 @@ public class PasswordGenerator {
                 String email = sc.next();
                 System.out.println("Master Password:");
                 String masterPassword = sc.next();
-                System.out.println("Character Set (default: d):");
-                String characterSet = sc.next();
+                ArrayList<CharacterGroup> options = new ArrayList<>();
+                for (int i = 0; i < groups.length; i++) {
+                    System.out.println(prompts[i]);
+                    if (sc.next().equalsIgnoreCase("y")) {
+                        options.add(groups[i]);
+                    }
+                }
                 System.out.println("Length:");
                 int length = sc.nextInt();
                 System.out.println("Your password:");
-                System.out.println(Generator.getPassword(email, masterPassword, length,characterSet));
-
+                System.out.println(new Generator(masterPassword,length,options,email));
                 System.out.print("Another? (Y/N): ");
             } while (sc.next().equalsIgnoreCase("y"));
         }catch (Exception e){
