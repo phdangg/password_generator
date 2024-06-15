97b3cc577ab7fc201e788c34db3b2dffec5cfaa6
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 13:24:07 +0700

    map character group return character set

--- PasswordGenerator/src/main/java/CharacterSet.java
+++ PasswordGenerator/src/main/java/CharacterSet.java
@@ -3,4 +3,18 @@ public class CharacterSet {
     public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
     public static final String SYMBOL = "#!\"~|@^°$%&/()[]{}=-_+*<>;:.";
     public static final String NUMBER = "0123456789";
+    public static String mapCharacterGroupToCharacterSet(CharacterGroup op) {
+        switch (op) {
+            case LOWER:
+                return CharacterSet.LOWER;
+            case NUMBER:
+                return CharacterSet.NUMBER;
+            case SYMBOL:
+                return CharacterSet.SYMBOL;
+            case UPPER:
+                return CharacterSet.UPPER;
+            default:
+                return "";
+        }
+    }
 }
