f384a7976f391410d23b638d85cee138bf351c68
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 13:48:10 +0700

    generate character set with list of character group options

--- PasswordGenerator/src/main/java/CharacterSet.java
+++ PasswordGenerator/src/main/java/CharacterSet.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class CharacterSet {
     public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
     public static final String SYMBOL = "#!\"~|@^°$%&/()[]{}=-_+*<>;:.";
     public static final String NUMBER = "0123456789";
+    public static String CHARACTER_SET;
     public static String mapCharacterGroupToCharacterSet(CharacterGroup op) {
         return switch (op) {
             case LOWER -> CharacterSet.LOWER;
@@ -11,5 +14,13 @@ public class CharacterSet {
             case UPPER -> CharacterSet.UPPER;
             default -> "";
         };
+    }
+    public static String generateCharacterSet(ArrayList<CharacterGroup> options){
+        StringBuilder characterSet = new StringBuilder();
+        for (CharacterGroup op : options) {
+            characterSet.append(CharacterSet.mapCharacterGroupToCharacterSet(op));
+        }
+        CHARACTER_SET = characterSet.toString();
+        return CHARACTER_SET;
     }
 }
