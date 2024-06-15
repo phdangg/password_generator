5ff5c35bf50a32ed832f19b473a728c2cf4d7269
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 13:24:33 +0700

    map character group return represent character

--- PasswordGenerator/src/main/java/CharacterGroup.java
+++ PasswordGenerator/src/main/java/CharacterGroup.java
@@ -1,3 +1,17 @@
 public enum CharacterGroup {
-    UPPER,LOWER,NUMBER,SYMBOL
+    UPPER,LOWER,NUMBER,SYMBOL;
+    public static String mapCharacterGroupToTemplate(CharacterGroup op) {
+        switch (op) {
+            case LOWER:
+                return "a";
+            case NUMBER:
+                return "n";
+            case SYMBOL:
+                return "s";
+            case UPPER:
+                return "A";
+            default:
+                return "";
+        }
+    }
 }
