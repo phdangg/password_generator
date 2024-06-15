6d28a57e5c0603adbdece8454fbb248b5958886b
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-15 13:58:06 +0700

    get map of Character group - character set

--- PasswordGenerator/src/main/java/CharacterSet.java
+++ PasswordGenerator/src/main/java/CharacterSet.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CharacterSet {
     public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
     public static final String SYMBOL = "#!\"~|@^°$%&/()[]{}=-_+*<>;:.";
     public static final String NUMBER = "0123456789";
+    public static String CHARACTER_SET;
     public static String mapCharacterGroupToCharacterSet(CharacterGroup op) {
         return switch (op) {
             case LOWER -> CharacterSet.LOWER;
@@ -20,5 +23,14 @@ public class CharacterSet {
             characterSet.append(CharacterSet.mapCharacterGroupToCharacterSet(op));
         }
         return characterSet.toString();
+    }
+    public static Map<Character,String> mapCharacterSet(){
+        Map<Character, String> templateToCharacterSetMap = new HashMap<>();
+        templateToCharacterSetMap.put('x', CHARACTER_SET);
+        templateToCharacterSetMap.put('a', LOWER);
+        templateToCharacterSetMap.put('A', UPPER);
+        templateToCharacterSetMap.put('s', SYMBOL);
+        templateToCharacterSetMap.put('n', NUMBER);
+        return templateToCharacterSetMap;
     }
 }
